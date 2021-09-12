40935dd5b3664416167a37d392ddf55007a966f1
titusowuor30 <titusowuor30@gmail.com>
2021-09-12 16:08:52 -0700

    initial commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,14 +32,9 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # ALLOWED_HOSTS = ['*']
-if DEBUG:
-        ALLOWED_HOSTS = ["localhost", "127.0.0.1","https://kimfittyportfolio.herokuapp.com/",]
-else:
-    ALLOWED_HOSTS = ['https://titusowuorportfolio.herokuapp.com/']
-
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","kimfittyportfolio.herokuapp.com/",]
 
 # Application definition
 
@@ -98,14 +93,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myportfolioapp.urls')),
     url(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
